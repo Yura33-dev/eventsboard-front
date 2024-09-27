c275209c2160d53bf821f17b48a5819f9e43602e
Yura Kasyan <33.yurakasyan@gmail.com>
2024-09-27 19:42:27 +0200

    bugfix: removes useEffect from Layout

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,24 +1,8 @@
 import { Outlet } from 'react-router-dom';
 import { Box } from '@mui/material';
 import Header from './Header';
-import { useEffect } from 'react';
 
 const Layout = () => {
-  useEffect(() => {
-    async function wakeupServer() {
-      fetch('https://eventsboard-back.onrender.com/events/')
-        .then((response) => response.json())
-        .catch((e) => console.log('Some error:', e));
-    }
-
-    const interval = setInterval(async () => {
-      await wakeupServer();
-    }, 23000);
-
-    return () => {
-      clearInterval(interval);
-    };
-  });
   return (
     <Box sx={{ display: 'flex', flexDirection: 'column' }}>
       <Header />
